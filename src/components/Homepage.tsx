5ad6e574f2ceb836a0d751fe57b795dd06848f2b
krishkashiwala <krishkashiwala@gmail.com>
2021-11-20 19:24:09 +0530

    responsive yet to be implemented.

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -23,22 +23,22 @@ export default function Homepage() {
 			<a href="mailto:krishkashiwala@gmail.com" className="fixed bottom-16  rounded-l-full bg-gray-900 py-4 px-4 -right-56 transform rotate-90 bg-transparent z-20 email-link">krishkashiwala@gmail.com <span className="text-white"> &nbsp;&nbsp;&nbsp;_________________________________________________________ </span></a>
 			<main className="flex justify-between mt-24 ">
 				<div className=" fixed bottom-0 ml-10 bg-transparent z-10 ">
-					<ul className="flex flex-col justify-between gap-5 h-full p-3 bg-gray-900 bg-transparent blur rounded-t-full">
-						<a href="https://github.com/KrishKashiwala" rel="noreferrer" target="_blank" className="media-links transform transition duration-300 ease-in hover:-translate-y-1"><i className="fab fa-github text-xl transform hover:scale-y-4"></i></a>
-						<a href="https://www.linkedin.com/in/krish-kashiwala-4384521ab/" rel="noreferrer" target="_blank" className="media-links transform transition duration-300 ease-in hover:-translate-y-1"><i className="fab fa-linkedin text-xl"></i></a >
-						<a href="https://www.instagram.com/krish.kashiwala/" target="_blank" rel="noreferrer" className="media-links transform transition duration-300 ease-in hover:-translate-y-1"><i className="fab fa-instagram text-xl"></i></a >
-						<a href="https://twitter.com/krishebish" target="_blank" rel="noreferrer" className="media-links transform transition duration-300 ease-in hover:-translate-y-1"><i className="fab fa-twitter text-xl"></i></a >
+					<ul className="flex flex-col justify-between gap-5 h-full p-4 bg-gray-900 bg-transparent blur rounded-t-full">
+						<a href="https://github.com/KrishKashiwala" rel="noreferrer" target="_blank" className="media-links transform transition duration-300 ease-in hover:-translate-y-1"><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-github w-5 h-5"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a>
+						<a href="https://www.linkedin.com/in/krish-kashiwala-4384521ab/" rel="noreferrer" target="_blank" className="media-links transform transition duration-300 ease-in hover:-translate-y-1"><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-linkedin"><title>LinkedIn</title><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg></a >
+						<a href="https://www.instagram.com/krish.kashiwala/" target="_blank" rel="noreferrer" className="media-links transform transition duration-300 ease-in hover:-translate-y-1"><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-instagram"><title>Instagram</title><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg></a >
+						<a href="https://twitter.com/krishebish" target="_blank" rel="noreferrer" className="media-links transform transition duration-300 ease-in hover:-translate-y-1"><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-twitter"><title>Twitter</title><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg></a >
 						<div className="ml-2 border-l-2 border-l-white h-24 "></div>
 					</ul>
 				</div>
 				<div className="flex ">
-					<img src="https://www.vectorlogo.zone/logos/github/github-tile.svg" className=" w-11  absolute top-28 left-52 top-down transform rotate-45" alt="Git-Hub-Mark-32px" />
-					<img src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg" className=" w-11 transform -rotate-45 bot-down absolute bottom-6 left-24" alt="Git-Hub-Mark-32px" />
+					<img src="https://www.vectorlogo.zone/logos/github/github-tile.svg" className=" w-11  absolute top-28 left-52 top-down transform rotate-45  " alt="Git-Hub-Mark-32px" />
+					<img src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg" className=" w-11 transform -rotate-45 bot-down absolute bottom-6 left-24 " alt="Git-Hub-Mark-32px" />
 					<img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Google_Chrome_icon_%28September_2014%29.svg" className="animate-spin w-11 absolute bottom-44 right-3  " alt="Git-Hub-Mark-32px" />
 					<img src="https://i.ibb.co/YcfmN8W/logo512.png" alt="logo512" className="w-11 absolute  top-50 animate-spin transform  scale-x-150 scale-y-150  right-72 up-down" ></img>
 					<img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="logo512" className="w-11 absolute bottom-64 right-64 transform rotate-45" ></img>
 					<img src="https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-icon.svg" alt="logo512" className="w-11 absolute top-32 right-24 " ></img>
-					<img src="https://www.vectorlogo.zone/logos/mongodb/mongodb-icon.svg" alt="logo512" className="w-11 absolute top-64 left-20" ></img>
+					<img src="https://www.vectorlogo.zone/logos/mongodb/mongodb-icon.svg" alt="logo512" className="w-11 absolute top-64 left-20 " ></img>
 					<img src="https://www.vectorlogo.zone/logos/vim/vim-icon.svg" alt="logo512" className="w-11 absolute bottom-20 bot-down left-3/4 transform -rotate-45" ></img>
 					<img src="https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg" alt="logo512" className="w-11 absolute mid-down top-96 left-52 transform -rotate-360   scale-x-150 scale-y-150 " ></img>
 					<img src="https://www.vectorlogo.zone/logos/yarnpkg/yarnpkg-icon.svg" alt="logo512" className="w-11 absolute  bottom-10 bot-down right-52 transform -rotate-45" ></img>
@@ -47,7 +47,7 @@ export default function Homepage() {
 					<div className="main-section ml-10 ">
 						<span className="font-normal text-base main-span-1 blur animate__animated animate__fadeInUp  ">Hi 👋🏻, my name is</span>
 						<h1 className="main-h1 animate__animated animate__fadeInUp">Krish Kashiwala.</h1>
-						<h3 className="w-3/4 main-h3 animate__animated animate__fadeInUp" >In ❤️ with  Technology, Photography and Humor.</h3>
+						<h3 className="w-3/4 main-h3 animate__animated animate__fadeInUp" >In <a title="(Bubinator), Public domain, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:Love_Heart_SVG.svg"><img className="w-9 h-9" alt="Love Heart SVG" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Love_Heart_SVG.svg/512px-Love_Heart_SVG.svg.png" /></a> with  Technology, Photography and Humor.</h3>
 						<div className="w-5/12 mt-4 mb-4 font-normal text-lg text-gray-600 animate__animated animate__fadeInUp">I'm a computer science grad based in Silicon Valley who thinks with both left and right brains whether it's coding, photography, or motion graphics.</div>
 					</div>
 					<a href="mailto:krishkashiwala@gmail.com" className="section-button rounded-lg  border-2 ml-10 mt-4 border-opacity-3  border-gray-50   w-52 p-4 box-border transition duration-400 ease-in transform  hover:bg-white  hover:text-black">Get In Touch 👋🏻</a>
@@ -55,10 +55,10 @@ export default function Homepage() {
 			</main>
 
 			<main id="about" >
-				<section className="mt-52 flex flex-col ml-36 mr-20 second-main" data-aos="fade-up" data-aos-once="true">
+				<section className="mt-52 flex flex-col ml-36 mr-20 second-main " data-aos="fade-up" data-aos-once="true">
 					<div className="flex flex-row items-center ">
-						<span className="font-bold info-head text-3xl animate__animated animate__fadeInUp">About Me&nbsp;&nbsp;</span>
-						<hr className="w-64 ml-2 opacity-40 animate__animated animate__fadeInUp" />
+						<span className="font-bold info-head text-3xl animate__animated animate__fadeInUp ">About Me&nbsp;&nbsp;</span>
+						<hr className="w-64 ml-2 opacity-40 animate__animated animate__fadeInUp " />
 					</div>
 					<br />
 					<div className="flex flex-row items-center  justify-around" >
@@ -114,18 +114,18 @@ export default function Homepage() {
 					</div >
 				</section >
 			</main >
-			<section className=" mt-52">
+			<section className=" mt-64" id="experience">
 				<div className="flex flex-row items-center  ml-64">
-					<h2 data-sr-id="3" className="visible animate__animated animate__fadeInUp opacity-100 text-3xl exp-h2">Experience</h2>
+					<h2 data-sr-id="3" className="visible animate__animated animate__fadeInUp opacity-100 text-3xl exp-h2 font-bold">Experience</h2>
 					<hr className="w-64 ml-4 text-gray-600  opacity-40 animate__animated animate__fadeInUp" />
 				</div>
 				<div className="slider">
 					<Experience />
 				</div>
 			</section>
-			<section id="projects" className="mt-64">
+			<section id="projects" className="mt-96">
 				<div className="flex flex-row items-center ml-64">
-					<h2 data-sr-id="3" className="visible animate__animated animate__fadeInUp opacity-100 text-3xl pro-h2">Some things I've Built</h2>
+					<h2 data-sr-id="3" className="visible animate__animated animate__fadeInUp opacity-100 text-3xl pro-h2 font-bold">Some things I've Built</h2>
 					<hr className="w-64 ml-4 text-gray-800  opacity-80 animate__animated animate__fadeInUp" />
 				</div>
 				<ul className="flex flex-row ">
@@ -135,19 +135,13 @@ export default function Homepage() {
 					</li>
 				</ul>
 			</section>
-			<section className="flex flex-col text-center pro  items-center  mt-44 ml-0 mr-0" data-aos="fade-up" id="projects" >
-				<h1 className="mb-10 text-3xl font-bold ">Projects</h1>
+			<section className="flex flex-col text-center pro  items-center  mt-96 ml-0 mr-0" data-aos="fade-up" id="projects" >
+				<h1 className="mb-10 text-3xl font-bold">Other Noteworthy Projects</h1>
 
 				<div className="flex flex-row  gap-10">
-					<Tilt scale={1.2} easing>
-						<Project title="Farmgistics" des="An online portal where farmers can sell their crops all over the nation without interference of farmer market." gitlink="https://github.com/KrishKashiwala/Farmgistics" extralink="" />
-					</Tilt>
-					<Tilt scale={1.2} easing>
-						<Project title="Chat App" des="It's a command line based python chat application which basically connects two users on a socket connection and transfer of message does happen." gitlink="https://github.com/KrishKashiwala/cli-based-chat-app-using-python" extralink="https://www.linkedin.com/posts/krish-kashiwala-4384521ab_github-krishkashiwalacli-based-chat-app-using-python-activity-6851449785937903616-qGqM" />
-					</Tilt>
-					<Tilt scale={1.2} easing >
-						<Project title="" des="0A1A2F" gitlink="" extralink="" />
-					</Tilt>
+					<Project clas title="Farmgistics" des="An online portal where farmers can sell their crops all over the nation without interference of farmer market." gitlink="https://github.com/KrishKashiwala/Farmgistics" extralink="" />
+					<Project title="Chat App" des="It's a command line based python chat application which basically connects two users on a socket connection and transfer of message does happen." gitlink="https://github.com/KrishKashiwala/cli-based-chat-app-using-python" extralink="https://www.linkedin.com/posts/krish-kashiwala-4384521ab_github-krishkashiwalacli-based-chat-app-using-python-activity-6851449785937903616-qGqM" />
+					<Project title="" des="0A1A2F" gitlink="" extralink="" />
 				</div>
 			</section>
 			<section className="flex flex-col justify-center items-center foot-main" data-aos="fade-up" data-aos-once="true" id="contact">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,14 @@
-import React from 'react'
 import '../css/navbar.css'
-import * as AOS from 'aos'
 import 'animate.css';
 import "aos/dist/aos.css";
 const Navbar = () => {
-	React.useEffect(() => {
-		AOS.init({
-			duration: 300,
-		})
-	}, [])
 	return (
-		<div className="nav-main flex flex-row justify-between top-5 ml-20 mr-20" data-aos="fade-up">
+		<div className="nav-main flex flex-row justify-between top-5 ml-20 mr-20 sticky animate__animated animate__fadeInDown" >
 			<section className="logo rounded bg-cover w-3 h-3">Hello</section>
 			<section className="flex flex-row justify-between w-3/12 ">
 				<a href="#about" className="nav-link cursor-pointer hover:text-white">About</a>
+				<a href="#experience" className="nav-link cursor-pointer hover:text-white">Experience</a>
 				<a href="#projects" className="nav-link cursor-pointer hover:text-white">Projects</a>
-				<a href="/blogs" className="nav-link cursor-pointer hover:text-white">Blogs</a>
 				<a href="#contact" className="nav-link cursor-pointer hover:text-white">Contact</a>
 			</section>
 			<a href="https://drive.google.com/file/d/1w3qbgyAChvafxCf8xXZvcQ8VhUZMOaxL/view" rel="noreferrer" target="_blank" className="nav-button rounded-lg overflow-hidden  border-2 border-opacity-3  border-gray-50 px-4 py-1 box-border transition duration-500 ease-in-out transform  hover:bg-white  hover:text-black">Resume </a>
